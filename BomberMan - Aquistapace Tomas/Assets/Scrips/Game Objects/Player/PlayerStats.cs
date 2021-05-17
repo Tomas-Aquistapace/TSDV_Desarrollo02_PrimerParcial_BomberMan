60d9b2c80ae2ff90a11d05ac3646edf5210b7ecf
Tomas <Aquista35@gmail.com>
2021-05-17 00:16:55 -0300

    Invulnerabilidad agregada

--- BomberMan - Aquistapace Tomas/Assets/Scrips/Game Objects/Player/PlayerStats.cs
+++ BomberMan - Aquistapace Tomas/Assets/Scrips/Game Objects/Player/PlayerStats.cs
@@ -6,20 +6,32 @@ public class PlayerStats : MonoBehaviour , Destroyable
 {
     public int lifes = 3;
     public int points;
+    public float invulnerabilityTime = 2f;
+    public bool invulnerable;
     public bool isDead;
+
+    private Renderer render;
 
     void Start()
     {
         points = 0;
+        invulnerable = false;
         isDead = false;
+
+        render = transform.GetComponent<Renderer>();
     }
 
     public void TakeDamage()
     {
-        SubtractLife();
+        if (!invulnerable)
+        {
+            SubtractLife();
 
-        if (lifes <= 0)
-            SetIsDead(true);
+            StartCoroutine(ReceiveDamage());
+
+            if (lifes <= 0)
+                SetIsDead(true);
+        }
     }
 
     private void SubtractLife()
@@ -35,5 +47,31 @@ public class PlayerStats : MonoBehaviour , Destroyable
     public void SetIsDead(bool state)
     {
         isDead = state;
+    }
+
+    IEnumerator ReceiveDamage()
+    {
+        float lenght = 0.5f;
+        float time = 0;
+        float colorTime = 0;
+
+        Color originalColor = render.material.color;
+        invulnerable = true;
+
+        while (time <= invulnerabilityTime)
+        {
+            render.material.color = Color.Lerp(originalColor, Color.white, colorTime);
+
+            colorTime = Mathf.PingPong(Time.time, lenght);
+
+            time += Time.deltaTime;
+
+            yield return null;
+        }
+
+        render.material.color = originalColor;
+        invulnerable = false;
+
+        yield return true;
     }
 }
