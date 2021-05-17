791e3b239c43d737780bc802006fa683664bcad9
Tomas <Aquista35@gmail.com>
2021-05-16 21:15:31 -0300

    La bomba daña al jugador

--- BomberMan - Aquistapace Tomas/Assets/Scrips/Bomb/DetonateBomb.cs
+++ BomberMan - Aquistapace Tomas/Assets/Scrips/Bomb/DetonateBomb.cs
@@ -34,6 +34,50 @@ public class DetonateBomb : MonoBehaviour
     {
         Debug.Log("exploté");
 
+        CollisionChecks(Vector3.forward);
+
+        CollisionChecks(-Vector3.forward);
+
+        CollisionChecks(-Vector3.right);
+
+        CollisionChecks(Vector3.right);
+        
         Destroy(this.gameObject);
+    }
+
+    void CollisionChecks(Vector3 direction)
+    {
+        RaycastHit hit;
+
+        Ray collisionRay = new Ray(transform.position, direction);
+
+        if (Physics.Raycast(collisionRay, out hit, blastRange))
+        {
+            // Colisiono con algo
+            Debug.Log("Colisiono con: " + hit.transform.name);
+
+            if (hit.transform.tag == "Player")
+            {
+                PlayerStats playerGO = hit.transform.GetComponent<PlayerStats>();
+
+                playerGO.ReceiveDamage();
+            }
+        }
+        else
+        {
+            // No colosiono con nada
+        }
+    }
+
+    // -------------------------------
+
+    void OnDrawGizmos()
+    {
+        Debug.DrawRay(transform.position, new Vector3(0, 0, blastRange), Color.red);
+        Debug.DrawRay(transform.position, new Vector3(0, 0, -blastRange), Color.red);
+        Debug.DrawRay(transform.position, new Vector3(blastRange, 0, 0), Color.red);
+        Debug.DrawRay(transform.position, new Vector3(-blastRange, 0, 0), Color.red);
+
+        Debug.DrawRay(transform.position + Vector3.up, new Vector3(0, -1, 0), Color.blue);
     }
 }
---
+++ BomberMan - Aquistapace Tomas/Assets/Scrips/Player/PlayerStats.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int lifes = 3;
+    public int points;
+    public bool isDead;
+
+    void Start()
+    {
+        points = 0;
+        isDead = false;
+    }
+
+    public void ReceiveDamage()
+    {
+        SubtractLife();
+
+        if(lifes <= 0)
+            SetIsDead(true);
+    }
+
+    private void SubtractLife()
+    {
+        lifes--;
+    }
+
+    public void SetPoints(int amount)
+    {
+        points = amount;
+    }
+
+    public void SetIsDead(bool state)
+    {
+        isDead = state;
+    }
+}
